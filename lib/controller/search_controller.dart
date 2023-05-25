5ac62b750f3fc336f221fe3c290894d004977e90
imronarrosid <imronbaru007@gmail.com>
2023-05-25 14:28:01 +0000

    ad search controller

---
+++ lib/controller/search_controller.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:tiktok_clone/constans.dart';
+import 'package:tiktok_clone/model/user.dart';
+
+class SearchUserController extends GetxController{
+  final Rx<List<User>> _searchedUser = Rx<List<User>>([]);
+
+  List<User> get searcheduser => _searchedUser.value;
+
+  searchUser(String searchText) async{
+    _searchedUser.bindStream(
+      firestore.collection('user').where('username',isLessThanOrEqualTo: searchText ).snapshots().map((QuerySnapshot query) {
+        List<User> retVal=[];
+        for (var element in query.docs) {
+            retVal.add(User.fromSnap(element)) ;          
+        }
+        return retVal;
+      })
+    );
+  }
+}
