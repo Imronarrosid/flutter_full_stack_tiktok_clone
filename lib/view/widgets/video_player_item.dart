0f0bc04a4e37cdced8d8fc8a0cd466b5a2e5df6f
imronarrosid <imronbaru007@gmail.com>
2023-05-26 08:42:15 +0000

    add play and pause button

--- lib/view/widgets/video_player_item.dart
+++ lib/view/widgets/video_player_item.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -12,15 +14,27 @@ class VideoPlayerItem extends StatefulWidget {
 }
 
 class _VideoPlayerItemState extends State<VideoPlayerItem> {
-
   late VideoPlayerController videoPlayerController;
+  bool play = true;
+  bool visibility = false;
+  double opacity = 0.0;
+  bool _isVideoEnded = false;
 
   @override
   void initState() {
     super.initState();
-    videoPlayerController= VideoPlayerController.network(widget.videoUrl)..initialize().then((value) {
-      videoPlayerController.play();
-      videoPlayerController.setVolume(1);
+    videoPlayerController = VideoPlayerController.network(widget.videoUrl)
+      ..initialize().then((value) {
+        videoPlayerController.play();
+        videoPlayerController.setVolume(1);
+      });
+    videoPlayerController.addListener(() {
+      final isVideoEnded = videoPlayerController.value.position >=
+          videoPlayerController.value.duration;
+      if (isVideoEnded) {
+        _isVideoEnded = true;
+        play = false;
+      }
     });
   }
 
@@ -29,18 +43,64 @@ class _VideoPlayerItemState extends State<VideoPlayerItem> {
     super.dispose();
     videoPlayerController.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Container(
-      width: size.width,
-      height: size.height,
-decoration: BoxDecoration(
-  color: Colors.black
-),
-child: VideoPlayer(videoPlayerController)
+    return GestureDetector(
+      onTap: () {
+        if (play && _isVideoEnded == false) {
+          setState(() {
+            videoPlayerController.pause();
+            play = false;
+            visibility = true;
 
-
+            opacity = 1;
+          });
+        } else if (_isVideoEnded && play == false) {
+          setState(() {
+            videoPlayerController.play();
+            play = true;
+            visibility = false;
+            opacity = 0;
+            _isVideoEnded=false;
+          });
+        } else if (play == false) {
+          setState(() {
+            videoPlayerController.play();
+            play = true;
+            visibility = false;
+            opacity = 0;
+            _isVideoEnded=false;
+          });
+        }
+      },
+      child: Stack(
+        children: [
+          Container(
+              width: size.width,
+              height: size.height,
+              decoration: const BoxDecoration(color: Colors.black),
+              child: VideoPlayer(videoPlayerController)),
+          Center(
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 300),
+              opacity: opacity,
+              child: Visibility(
+                  replacement: const Icon(
+                    Icons.pause_rounded,
+                    color: Colors.white,
+                    size: 25,
+                  ),
+                  visible: visibility,
+                  child: const Icon(
+                    Icons.play_arrow,
+                    color: Colors.white,
+                  )),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
