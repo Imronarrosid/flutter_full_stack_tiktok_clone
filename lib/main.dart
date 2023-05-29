0df1ee18a22a0c93361629f31133aead0f21c242
imronarrosid <imronbaru007@gmail.com>
2023-05-29 15:15:19 +0000

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/get_navigation.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:tiktok_clone/controller/auth_controller.dart';
 import 'package:tiktok_clone/view/screen/auth/signup_screen.dart';
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value){
+  await Firebase.initializeApp().then((value) {
     Get.put(AuthController());
   });
-   await initializeDateFormatting('id_ID', null).then((_) => runApp(const MyApp()));
+  await initializeDateFormatting('id_ID', null)
+      .then((_) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -21,8 +20,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    
     return GetMaterialApp(
       title: 'Tiktiok Clone',
       theme: ThemeData(
@@ -33,4 +30,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
