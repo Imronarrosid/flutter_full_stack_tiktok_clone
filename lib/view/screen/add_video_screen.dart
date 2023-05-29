a1b4cd8a1fa9bd1221bccc55567e0ff5cee11cb8
imronarrosid <imronbaru007@gmail.com>
2023-05-29 15:16:06 +0000

    add const modifier

--- lib/view/screen/add_video_screen.dart
+++ lib/view/screen/add_video_screen.dart
@@ -7,60 +7,66 @@ import 'package:tiktok_clone/view/screen/confirm_creen.dart';
 class AddVideoScreen extends StatelessWidget {
   const AddVideoScreen({super.key});
 
-    picVideo(ImageSource source,BuildContext context) async {
-      final galleryVideo =
-          await ImagePicker().pickVideo(source: source);
-          if (galleryVideo !=null && context.mounted) {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (_)=> ConfirmScreen(videeFile: File(galleryVideo.path), videoPath: galleryVideo.path,))
-            );
-          }
+  picVideo(ImageSource source, BuildContext context) async {
+    final galleryVideo = await ImagePicker().pickVideo(source: source);
+    if (galleryVideo != null && context.mounted) {
+      Navigator.of(context).push(MaterialPageRoute(
+          builder: (_) => ConfirmScreen(
+                videeFile: File(galleryVideo.path),
+                videoPath: galleryVideo.path,
+              )));
     }
-    showOptionDialog(BuildContext context){
-      return showDialog(context: context, builder: (_)=>
-      SimpleDialog(
-        children: [
-          SimpleDialogOption(
-            onPressed: ()=>picVideo(ImageSource.gallery, context),
-            child: Row(
+  }
+
+  showOptionDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (_) => SimpleDialog(
               children: [
-              Icon(Icons.image),
-              Text('Galeri'),
+                SimpleDialogOption(
+                  onPressed: () => picVideo(ImageSource.gallery, context),
+                  child: const Row(
+                    children: [
+                      Icon(Icons.image),
+                      Text('Galeri'),
+                    ],
+                  ),
+                ),
+                SimpleDialogOption(
+                  onPressed: () => picVideo(ImageSource.camera, context),
+                  child: const Row(
+                    children: [
+                      Icon(Icons.camera_alt),
+                      Text('Kamera'),
+                    ],
+                  ),
+                ),
+                SimpleDialogOption(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Row(
+                    children: [
+                      Icon(Icons.cancel),
+                      Text('Batal'),
+                    ],
+                  ),
+                ),
               ],
-            ),
-          ),
-          SimpleDialogOption(
-            onPressed: ()=>picVideo(ImageSource.camera, context),
-            child: Row(
-              children: [
-              Icon(Icons.camera_alt),
-              Text('Kamera'),
-              ],
-            ),
-          ),
-          SimpleDialogOption(
-            onPressed: ()=>Navigator.pop(context),
-            child: Row(
-              children: [
-              Icon(Icons.cancel),
-              Text('Batal'),
-              ],
-            ),
-          ),
-        ],
-      )
-      );
-    }
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(title:const Text('Upload video') ,),
+      appBar: AppBar(
+        title: const Text('Upload video'),
+      ),
       body: Container(
-        color: Colors.white,
-        child:  Center(
-        child: ElevatedButton(onPressed: ()=>showOptionDialog(context), child: const Text('Upload vidio')),
-      )),
+          color: Colors.white,
+          child: Center(
+            child: ElevatedButton(
+                onPressed: () => showOptionDialog(context),
+                child: const Text('Upload vidio')),
+          )),
     );
   }
 }
