5040f1a22c0dc06075d4cf63f472b6d4637224bc
imronarrosid <imronbaru007@gmail.com>
2023-05-29 13:51:55 +0000

    add validator

--- lib/view/screen/comment_screen.dart
+++ lib/view/screen/comment_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tiktok_clone/constans.dart';
@@ -11,8 +10,7 @@ class CommentScreeen extends StatelessWidget {
 
   final TextEditingController _commentConteoller = TextEditingController();
   final CommentController commentController = Get.put(CommentController());
-
-
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -20,95 +18,119 @@ class CommentScreeen extends StatelessWidget {
     commentController.updatePostId(id);
     tago.setLocaleMessages('id', tago.IdMessages());
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-            width: size.width,
-            height: size.height,
-            child: Column(
-              children: [
-                Expanded(child: Obx(() {
-
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: commentController.comments.length,
-                    itemBuilder: (context, index) {
-                  final comment = commentController.comments[index];
-                  print(commentController.comments.length);
-                    return ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.black,
-                        backgroundImage: NetworkImage(comment.profileImg),
-                      ),
-
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            comment.username,
-                            style: const TextStyle(
-                                fontSize: 20,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          Text(comment.comment,style: TextStyle(height: 0.5),)
-                        ],
-                      ),
-                      subtitle: Row(
-                        children: [
-                          Text(
-                          tago.format(DateTime.parse(comment.datePublished.toDate().toString()),locale: 'id').toString(),
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          
-                        ],
-                      ),
-                      
-                      trailing: Column(
-                        children: [
-                          InkWell(
-                            onTap: ()=> commentController.likeComment(comment.id),
-                            child: comment.likes.contains(authController.user.uid)? const Icon(Icons.favorite ,color: Colors.red,): const Icon(Icons.favorite_border_outlined)),
-                          Text(
-                            comment.likes.length.toString(),
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    );
-                  });
-                })),
-                const Divider(),
-                ListTile(
-                  title: TextFormField(
-                    controller: _commentConteoller,
-                    decoration: InputDecoration(
-                      labelText: 'Comment',
-                      labelStyle: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w700),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue),
-                      ),
-                    ),
+      appBar: AppBar(),
+      body: Container(
+        color: Colors.white,
+        child: Obx(() {
+          return ListView.builder(
+              shrinkWrap: true,
+              itemCount: commentController.comments.length,
+              itemBuilder: (context, index) {
+                final comment = commentController.comments[index];
+                print(commentController.comments.length);
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.black,
+                    backgroundImage: NetworkImage(comment.profileImg),
                   ),
-                  trailing: TextButton(
-                    child: Text('Kirim'),
-                    onPressed: () =>
-                        commentController.postComment(_commentConteoller.text),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        comment.username,
+                        style: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        comment.comment,
+                        style: const TextStyle(height: 0.5),
+                      )
+                    ],
                   ),
-                )
-              ],
-            )),
+                  subtitle: Row(
+                    children: [
+                      Text(
+                        tago
+                            .format(
+                                DateTime.parse(
+                                    comment.datePublished.toDate().toString()),
+                                locale: 'id')
+                            .toString(),
+                        style:
+                            const TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                    ],
+                  ),
+                  trailing: Column(
+                    children: [
+                      InkWell(
+                          onTap: () =>
+                              commentController.likeComment(comment.id),
+                          child: comment.likes.contains(authController.user.uid)
+                              ? const Icon(
+                                  Icons.favorite,
+                                  color: Colors.red,
+                                )
+                              : const Icon(Icons.favorite_border_outlined)),
+                      Text(
+                        comment.likes.length.toString(),
+                        style:
+                            const TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                );
+              });
+        }),
       ),
+      persistentFooterButtons: [
+        Container(
+          margin: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom + 5),
+          child: Form(
+            key: _formKey,
+            child: ListTile(
+              title: TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Tidak boleh kosong';
+                  }
+                  return null;
+                },
+                controller: _commentConteoller,
+                decoration: const InputDecoration(
+                  hintText: 'Tulis komentar',
+                  labelStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                ),
+              ),
+              trailing: TextButton(
+                  child: const Text('Kirim'),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      commentController.postComment(_commentConteoller.text);
+                      _commentConteoller.clear();
+                    }
+                  }),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
