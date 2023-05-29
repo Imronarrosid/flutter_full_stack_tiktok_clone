d8b27b89340d6ece6a9894a6c0afc0ab3ac83f97
imronarrosid <imronbaru007@gmail.com>
2023-05-29 15:07:43 +0000

    remove unused code

--- lib/controller/upload_video_controller.dart
+++ lib/controller/upload_video_controller.dart
@@ -8,17 +8,10 @@ import 'package:tiktok_clone/model/video.dart';
 import 'package:video_compress/video_compress.dart';
 
 class UploadVideoController extends GetxController {
- _compressVideo(String videoPath) async {
-      
-      //Get id
-      var allDocs = await firestore.collection('video').get();
-      int len = allDocs.docs.length;
-    // final comress = compressor.LightCompressor();
-    // dynamic response = comress.compressVideo(path: videoPath, videoQuality: compressor.VideoQuality.medium, android: compressor.AndroidConfig(isSharedStorage: true, saveAt: compressor.SaveAt.Movies), ios: compressor.IOSConfig(saveInGallery: true), video: compressor.Video(videoName: 'video$len'),);
+  _compressVideo(String videoPath) async {
     var comressedVideo = await VideoCompress.compressVideo(videoPath,
-        quality: VideoQuality.MediumQuality,
-        includeAudio: true);
-    return comressedVideo != null ? comressedVideo.file:File(videoPath);
+        quality: VideoQuality.MediumQuality, includeAudio: true);
+    return comressedVideo != null ? comressedVideo.file : File(videoPath);
     // return response;
   }
 
@@ -41,7 +34,7 @@ class UploadVideoController extends GetxController {
 
     UploadTask uploadTask = ref.putFile(await _getThumnaile(videoPath));
     TaskSnapshot snapshot = await uploadTask;
-    if(snapshot.state == TaskState.success){
+    if (snapshot.state == TaskState.success) {
       Get.snackbar('Upload vidio berhasil', '');
     }
     String downloaUrl = await snapshot.ref.getDownloadURL();
@@ -49,34 +42,36 @@ class UploadVideoController extends GetxController {
   }
 
   //Upload video
-  uploapVideo(String songName, String caption, String videoPath,File videoFile) async {
+  uploapVideo(
+      String songName, String caption, String videoPath, File videoFile) async {
     try {
-          Get.back();
-          Get.back();
+      Get.back();
+      Get.back();
       String uid = firebaseAuth.currentUser!.uid;
       DocumentSnapshot userDoc =
           await firestore.collection('user').doc(uid).get();
       //Get id
       var allDocs = await firestore.collection('videos').get();
-      int len = allDocs.docs.length; 
-      String videoUrl = await _uploadToStorage("video $len", await _compressVideo(videoPath));
+      int len = allDocs.docs.length;
+      String videoUrl =
+          await _uploadToStorage("video $len", await _compressVideo(videoPath));
       String thumnail =
           await _uploadThumnailesToStorage("video $len", videoPath);
-      
+
       Video video = Video(
-          username:  (userDoc.data()! as Map<String , dynamic>)['username'],
+          username: (userDoc.data()! as Map<String, dynamic>)['username'],
           uid: uid,
           id: "video$len",
           songName: songName,
           caption: caption,
           thumnail: thumnail,
           videoUrl: videoUrl,
-          profileImg:  (userDoc.data()! as Map<String , dynamic>)['profileImg'],
+          profileImg: (userDoc.data()! as Map<String, dynamic>)['profileImg'],
           likes: [],
           commentCount: 0,
           shareCount: 0);
 
-          await firestore.collection('videos').doc('video$len').set(video.toJson());
+      await firestore.collection('videos').doc('video$len').set(video.toJson());
     } catch (e) {
       Get.snackbar('Upload video error', e.toString());
     }
